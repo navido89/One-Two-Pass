02bec152e6f829f72bb4490aa1c5c6f20da5e232
Navid Mashinchi <nmashinchi@gmail.com>
2019-03-14 18:21:40 -0700

    Assignment Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes: static", ()=> {
 
@@ -8,6 +9,16 @@ describe("routes: static", ()=> {
 
         it ("should return status code 200", (done)=> {
             request.get(base, (err,res,body)=> {
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+    });
+
+    describe("GET /marco", ()=>{
+        
+        it ("should return status code 200", (done)=>{
+            request.get(marco, (err,res,body)=> {
                 expect(res.statusCode).toBe(200);
                 done();
             });
