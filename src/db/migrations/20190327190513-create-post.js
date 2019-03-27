aac85e74822166bfa51054387532cdc7c5f010b9
Navid Mashinchi <nmashinchi@gmail.com>
2019-03-27 15:40:25 -0700

    Checkpoint Post Resource Part2 Completed

--- src/db/migrations/20190327190513-create-post.js
+++ src/db/migrations/20190327190513-create-post.js
@@ -29,7 +29,7 @@ module.exports = {
         onDelete: "CASCADE",
         allowNull: false,
         references: {
-          model: "Topic",
+          model: "Topics",
           key: "id",
           as: "topicId"
         },
