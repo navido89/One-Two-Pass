1f9e4e32d8164e5a0bdcd647fa938958442b030c
Navid Mashinchi <nmashinchi@gmail.com>
2019-04-01 16:43:51 -0700

    topic_spec.js tests failing

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,10 +3,12 @@ module.exports = {
         const staticRoutes = require ("../routes/static");
         const topicRoutes = require("../routes/topics");
         const advertisementRoutes = require("../routes/advertisements");
+        const postRoutes = require ("../routes/posts");
 
         app.use(staticRoutes);
         app.use(topicRoutes);
         app.use(advertisementRoutes);
+        app.use(postRoutes);
 
     }
 }
