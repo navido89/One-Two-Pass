e6925e882dbdd1c129624b62db3987b8a5785dd8
Navid Mashinchi <nmashinchi@gmail.com>
2019-04-08 11:25:01 -0700

    Master Updated

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,21 +4,17 @@ module.exports = {
         const topicRoutes = require("../routes/topics");
         const advertisementRoutes = require("../routes/advertisements");
         const postRoutes = require ("../routes/posts");
-<<<<<<< HEAD
+
         const flairRoutes = require ("../routes/flairs");
-=======
+
         const userRoutes = require("../routes/users");
->>>>>>> Authentication-Checkpoint
+
 
         app.use(staticRoutes);
         app.use(topicRoutes);
         app.use(advertisementRoutes);
         app.use(postRoutes);
-<<<<<<< HEAD
         app.use(flairRoutes);
-=======
-        app.use(userRoutes);
->>>>>>> Authentication-Checkpoint
 
     }
 }
