662b8e6d9ef3490913bcd5ae31b5b41b044ad554
Navid Mashinchi <nmashinchi@gmail.com>
2019-04-09 19:28:11 -0700

    Authorization Checkpoint Almost Done

--- spec/unit/post_spec.js
+++ spec/unit/post_spec.js
@@ -1,49 +1,60 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
 
 describe("Post", () => {
 
     beforeEach((done)=> {
         this.topic;
         this.post;
+        this.user;
+
         sequelize.sync({force: true}).then((res) => {
 
-            Topic.create({
-                title: "Expeditions to Alpha Centauri",
-                description: "A compilation of reports from recent visits to the star system."
+            User.create({
+                email: "starman@tesla.com",
+                password: "Trekkie4lyfe"
             })
-            .then((topic) => {
-                this.topic = topic;
+            .then((user) => {
+                this.user = user;
 
-                Post.create({
-                    title: "My first visit to Proxima Centauri b",
-                    body: "I saw some rocks.",
-                    topicId: this.topic.id
+                Topic.create({
+                    title: "Expeditions to Alpha Centauri",
+                    description: "A compilation of reports from recent visits to the stay system.",
+                    posts: [{
+                        title: "My first visit to Proxima Centauri b",
+                        body: "I saw some rocks",
+                        userId: this.user.id
+                    }]
+                }, {
+                    include: {
+                        model: Post,
+                        as: "posts"
+                    }
                 })
-                .then((post)=> {
-                    this.post = post;
+                .then((topic) => {
+                    this.topic = topic;
+                    this.post = topic.posts[0];
                     done();
-                });
+                })
             })
-            .catch((err) => {
-                console.log(err);
-                done();
-            });
         });
     });
 
 describe("#create()", () => {
-    it("should create a post object with a title,body, and assigned topic", (done) => {
+    it("should create a post object with a title,body, and assigned topic and user", (done) => {
         
         Post.create({
             title: "Pros of Cryosleep during the long journey",
             body: "1. Not having to answer the 'are there yet?' question.",
-            topicId: this.topic.id
+            topicId: this.topic.id,
+            userId: this.user.id
         })
         .then((post)=> {
             expect(post.title).toBe("Pros of Cryosleep during the long journey");
             expect(post.body).toBe("1. Not having to answer the 'are there yet?' question.");
+            expect(post.userId).toBe(this.user.id);
             done();
         })
         .catch((err)=>{
@@ -102,4 +113,42 @@ describe("#getTopic()", ()=>{
         });
     });
 });
+
+describe("#setUser()", () => {
+
+    it("should associate a post and a user together", (done) => {
+
+      User.create({
+        email: "ada@example.com",
+        password: "password"
+      })
+      .then((newUser) => {
+
+        expect(this.post.userId).toBe(this.user.id);
+
+        this.post.setUser(newUser)
+        .then((post) => {
+
+          expect(this.post.userId).toBe(newUser.id);
+          done();
+
+        });
+      })
+    });
+
+  });
+
+  describe("#getUser()", () => {
+
+    it("should return the associated topic", (done) => {
+
+      this.post.getUser()
+      .then((associatedUser) => {
+        expect(associatedUser.email).toBe("starman@tesla.com");
+        done();
+      });
+
+    });
+
+  });
 });
--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,40 +1,53 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
+
 describe("Topic", () => {
 
   beforeEach((done) => {
-    
     this.topic;
     this.post;
+    this.user;
+
     sequelize.sync({force: true}).then((res) => {
 
-      
-      Topic.create({
-        title: "World Cup",
-        description: "This is a review of a soccer tournament"
-      })
-      .then((topic) => {
-        this.topic = topic;
-        
-        Post.create({
-          title: "My thoughts",
-          body: "I think it was fun",
-          //#4
-          topicId: this.topic.id
-        })
-        .then((post) => {
-          this.post = post;
-          done();
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        done();
-      });
-    });
 
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
+      })
+      .then((user) => {
+        this.user = user; 
+
+
+        Topic.create({
+          title: "Expeditions to Alpha Centauri",
+          description: "A compilation of reports from recent visits to the star system.",
+
+
+          posts: [{
+            title: "My first visit to Proxima Centauri b",
+            body: "I saw some rocks.",
+            userId: this.user.id
+          }]
+        }, {
+
+
+          include: {
+            model: Post,
+            as: "posts"
+          }
+        })
+        .then((topic) => {
+          this.topic = topic; //store the topic
+          this.post = topic.posts[0]; //store the post
+          done();
+        })
+      })
+    });
   });
+
   describe("#create()", () => {
     it("should create a topic object with a title,and description", (done) => {
       Topic.create({
@@ -61,7 +74,8 @@ describe("Topic", () => {
       Post.create({
         title: "NBA Review",
         body: "This is a NBA Season review",
-        topicId:this.topic.id
+        topicId:this.topic.id,
+        userId: this.user.id
       })
 
       .then((newPost) => {
@@ -72,7 +86,7 @@ describe("Topic", () => {
           expect(post.topicId).toBe(this.topic.id);
           this.topic.getPosts()
           .then((associatedPosts) => {
-            expect(associatedPosts[0].title).toBe("My thoughts"
+            expect(associatedPosts[0].title).toBe("My first visit to Proxima Centauri b"
           );
           expect(associatedPosts[1].title).toBe("NBA Review")
 
