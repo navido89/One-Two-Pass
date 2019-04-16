77de08e8ee889d1c0fc7d58279885a4b50cbf3b5
Navid Mashinchi <nmashinchi@gmail.com>
2019-04-16 13:02:35 -0700

    Voting Checkpoint Completed

--- src/db/models/vote.js
+++ src/db/models/vote.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  var Vote = sequelize.define('Vote', {
+  var Vote = sequelize.define("Vote", {
     value: {
       type: DataTypes.INTEGER,
       allowNull: false,
       validate: {
-        isIn: [[-1,1]]
+        isIn: [[-1, 1]]
       }
     },
     postId: {
       type: DataTypes.INTEGER,
-      allowNUll: false
+      allowNull: false
     },
     userId: {
       type: DataTypes.INTEGER,
--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -5,6 +5,7 @@ const Comment = require("./models").Comment;
 const User = require("./models").User;
 
 const Authorizer = require("../policies/post");
+const Vote = require("./models").Vote;
 
 
 module.exports = {
@@ -23,7 +24,7 @@ module.exports = {
           include: [
             {model: Comment, as: "comments", include: [
               {model: User }
-            ]}
+            ]}, {model: Vote, as: "votes"}
           ]
         })
         .then((post) => {
