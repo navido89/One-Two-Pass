ad5663b8e265d6bb1fba71841b00fcc3be7e0495
Navid Mashinchi <nmashinchi@gmail.com>
2019-07-09 13:47:12 -0700

    Changed the design

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,7 +1,7 @@
 module.exports = {
     
     index(req, res, next){
-        res.render("static/index", {title: "Welcome to Bloccit"});
+        res.render("static/index", {title: "Welcome to OneTwoPass"});
     },
     
     about(req, res, next){
